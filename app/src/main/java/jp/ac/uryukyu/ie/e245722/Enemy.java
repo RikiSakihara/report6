30851157ad0036749f8eda60c288ebb92c38ffa4
RIki Sakihara <e245722@ie.u-ryukyu.ac.jp>
2025-01-09 14:22:15 +0900

    1st commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245722/Enemy.java
@@ -0,0 +1,26 @@
+package jp.ac.uryukyu.ie.e245722;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Enemy extends Character{
+    Enemy(String name, int Life) {
+        super(name , Life);
+        
+    }
+ 
+    @Override
+    void act(ArrayList<Character> targets) {
+
+        //選択肢を用意する
+
+        //actionsにいろいろ入れる・・・
+        var rand = new Random(); //修正必要あり
+        // Randonクラスのインスタンスを生成した時点で特定のシード値が使われる。つまり同じ値が得られる。（最初の数字はランダムではなくなる）
+        
+        int index = rand.nextInt(actions.size()); //ランダムに選択されたインデクス
+        var selected_action = actions.get(index);
+        
+        //どうにかしないといけない
+        selected_action.execute(this, targets.get(0));
+    }
+}
