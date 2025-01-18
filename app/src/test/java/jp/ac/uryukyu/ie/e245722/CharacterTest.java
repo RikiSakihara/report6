39a65461a60ee60aee16d4b7d0a5301ee0f43001
RIki Sakihara <e245722@ie.u-ryukyu.ac.jp>
2025-01-18 22:41:10 +0900

    2th commit

--- app/src/test/java/jp/ac/uryukyu/ie/e245722/CharacterTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e245722/CharacterTest.java
@@ -8,15 +8,18 @@ public class CharacterTest {
      * Character内のisAttackBarrierクラスが想定通りに動くかどうかの確認
      * 検証手順
      * (1)PlayerとEnemyを準備する。この時 ChargeCountはそれぞれ0とする。またLifeは10とする。
-     * (2)この時に,Player：かめはめ波　,　Enemy：チャージ　と行動するようにする。
+     * (2)Player：かめはめ波　,　Enemy：チャージ　と行動するようにする。
      * 　　→Playerの攻撃が失敗する　かつ　PlayerのChargeCountが0のままであることを期待
-     * (3)次に,Player,EnemyのChargeCountを100に設定し直す。
-     * (4)Player：元気玉　,　Enemy：かめはめ波　と行動するようにする。
+     * (3)PlayerのChargeCountを100に設定する。
+     * (4)Player : かめはめ波　, Enemy:かめはめ波　と行動するようにする。
+     * 　　→Playerの攻撃が成功することを期待
+     * (5)EnemyのChargeCountを100にする。
+     * (6)Player：元気玉　,　Enemy：かめはめ波　と行動するようにする。
      * 　　→EnemyのLifeのみが１減少していることを期待
-     * (5)Player：かめはめ波　,Enemy：かめはめ波　と行動するようにする。
+     * (7)Player：かめはめ波　,Enemy：かめはめ波　と行動するようにする。
      * 　　→どちらもLifeが減少しないことを期待
-     * (6)Player：かめはめ波　,　Enemy：バリア　と行動するようにする。
-     * 　　→どちらもLifeが減少しないことを期待
+     * (8)Player：かめはめ波　,　Enemy：バリア　と行動するようにする。
+     *  　→どちらもLifeが減少しないことを期待
      */
     @Test
     void isAttackBarrierTest() {
@@ -33,23 +36,30 @@ public class CharacterTest {
         assertEquals(Enemy.getLife(), Life);
 
         //(3)
-        Player.Chrage(100);
-        Enemy.Chrage(100);
+        Player.Charge(100);
 
         //(4)
-        TestChackmethod(Player, 3 , Enemy, 2 );
-        assertEquals(Player.getLife() , Life);
-        assertEquals(Enemy.getLife(), (int)(Life-1));
+        TestChackmethod(Player, 2, Enemy, 2);
+        assertEquals(Player.getLife(), Life);
+        assertEquals(Enemy.getLife(), Life-1);
 
         //(5)
-        TestChackmethod(Player, 2, Enemy, 2);
-        assertEquals(Player.getLife() , Life);
-        assertEquals(Enemy.getLife(), (int)(Life-1));
+        Enemy.Charge(100);
 
         //(6)
+        TestChackmethod(Player, 3 , Enemy, 2 );
+        assertEquals(Player.getLife() , Life);
+        assertEquals(Enemy.getLife(), Life-2);
+
+        //(7)
+        TestChackmethod(Player, 2, Enemy, 2);
+        assertEquals(Player.getLife() , Life);
+        assertEquals(Enemy.getLife(), Life-2);
+
+        //(8)
         TestChackmethod(Player, 2, Enemy, 0);
         assertEquals(Player.getLife() , Life);
-        assertEquals(Enemy.getLife(), (int)(Life-1));
+        assertEquals(Enemy.getLife(), Life-2);
     }
 
     void TestChackmethod(Character Player ,int PlayerComand , Character Enemy , int EnemyComand ){
@@ -62,7 +72,7 @@ public class CharacterTest {
         Player.getAction().get(PlayerComand).act(Player);
         Enemy.getAction().get(EnemyComand).act(Enemy);
         
-        Player.isAttackBarrier(Enemy , judgeX);
-        Enemy.isAttackBarrier(Player , judgeY);
+        Player.isAttackBarrier(Enemy , judgeX , judgeY);
+        Enemy.isAttackBarrier(Player , judgeY , judgeY);
     }
 }
